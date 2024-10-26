40dd4a68af988a1a41cd3b4af1cf951d1dabf5e3
gtbarbosa <gtbarbosa@live.com>
2024-10-26 15:34:23 -0300

    init repo api cliente

---
+++ build/generated/sources/annotationProcessor/java/main/br/com/fiap/techchallenge/lanchonete/adapters/mappers/ClienteMapperImpl.java
@@ -0,0 +1,61 @@
+package br.com.fiap.techchallenge.lanchonete.adapters.mappers;
+
+import br.com.fiap.techchallenge.lanchonete.dtos.inbound.ClienteDTO;
+import br.com.fiap.techchallenge.lanchonete.entities.Cliente;
+import br.com.fiap.techchallenge.lanchonete.entities.dbEntities.ClienteEntity;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-10-26T15:30:52-0300",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 17.0.12 (Ubuntu)"
+)
+@Component
+public class ClienteMapperImpl implements ClienteMapper {
+
+    @Override
+    public Cliente toEntity(ClienteDTO clienteRequest) {
+        if ( clienteRequest == null ) {
+            return null;
+        }
+
+        Cliente cliente = new Cliente();
+
+        cliente.setNome( clienteRequest.getNome() );
+        cliente.setEmail( clienteRequest.getEmail() );
+        cliente.setCpf( clienteRequest.getCpf() );
+
+        return cliente;
+    }
+
+    @Override
+    public ClienteEntity toDbEntity(Cliente cliente) {
+        if ( cliente == null ) {
+            return null;
+        }
+
+        ClienteEntity clienteEntity = new ClienteEntity();
+
+        clienteEntity.setNome( cliente.getNome() );
+        clienteEntity.setEmail( cliente.getEmail() );
+        clienteEntity.setCpf( cliente.getCpf() );
+
+        return clienteEntity;
+    }
+
+    @Override
+    public Cliente fromDbEntityToEntity(ClienteEntity clienteEntity) {
+        if ( clienteEntity == null ) {
+            return null;
+        }
+
+        Cliente cliente = new Cliente();
+
+        cliente.setNome( clienteEntity.getNome() );
+        cliente.setEmail( clienteEntity.getEmail() );
+        cliente.setCpf( clienteEntity.getCpf() );
+
+        return cliente;
+    }
+}
---
+++ src/main/java/br/com/fiap/techchallenge/lanchonete/entities/MensagemErroPadrao.java
@@ -0,0 +1,10 @@
+package br.com.fiap.techchallenge.lanchonete.entities;
+
+public class MensagemErroPadrao {
+
+    public static final String CLIENTE_CADASTRADO = "Cliente já cadastrado.";
+    public static final String CLIENTE_NAO_ENCONTRADO_CPF = "Cliente não encontrado para o cpf %s";
+    public static final String CLIENTE_NAO_ENCONTRADO_ATUALIZACAO = "Cliente não encontrado para atualização.";
+    public static final String ERRO_GENERICO = "Houve um erro. Tente novamente.";
+    public static final String LISTA_CLIENTE_VAZIA = "Nenhum cliente cadastrado.";
+}
