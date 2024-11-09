ece0494c251260bec0ffcb1df2a8fa311dbac662
gtbarbosa <gtbarbosa@live.com>
2024-11-09 16:00:09 -0300

    Ajustes do cucumber

--- src/test/java/br/com/fiap/techchallenge/lanchonete/bdd/CucumberTest.java
+++ src/test/java/br/com/fiap/techchallenge/lanchonete/bdd/CucumberTest.java
@@ -5,9 +5,9 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+@CucumberContextConfiguration
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@CucumberContextConfiguration
 public class CucumberTest {
 }
